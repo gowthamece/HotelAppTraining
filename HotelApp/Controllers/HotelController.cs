888af3970686f5b1d397e4a8418543e47fb9985a
Gowtham <gowthamk91@outlook.com>
2023-01-12 16:48:54 +0530

    Add project files.

---
+++ HotelApp/Controllers/HotelController.cs
@@ -0,0 +1,22 @@
+﻿using HotelApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelApp.Controllers
+{
+    public class HotelController : Controller
+    {
+        // GET: Hotel
+        public ActionResult Index()
+        {
+            var hotelList = new List<hotel>();
+            hotelList.Add(new hotel { Id = 1,Name="A2B" });
+            hotelList.Add(new hotel { Id = 1, Name = "Sachin" });
+            hotelList.Add(new hotel { Id = 1, Name = "Arun" });
+            return View(hotelList);
+        }
+    }
+}
---
+++ HotelApp/Models/hotel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelApp.Models
+{
+    public class hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+    }
+}
